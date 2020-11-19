1ebf97d12626957768660442e6532b07c4494192
NandeeshG <nandeeshgupta@gmail.com>
2020-11-19 16:36:13 +0530

    rules to student added. Now work on localStorage and list display and home page display. Then go to teachers section. Then try to deploy today itself on netlify or azure VM

--- pages/student.vue
+++ pages/student.vue
@@ -5,24 +5,52 @@
         <v-row justify="space-around">
           <v-col cols="9">
             <h1 class="text-h3 font-italic text-left mb-4">Students</h1>
-            <v-form class="mt-13">
+            <v-form ref="form" class="mt-13" @submit.prevent="addStudent">
               <v-row justify="center">
                 <v-col cols="12" sm="6">
-                  <v-text-field dense outlined label="Name" class="ma-n2" />
+                  <v-text-field
+                    v-model="newStudent.name"
+                    :rules="[rules.required, rules.name]"
+                    dense
+                    outlined
+                    label="Name"
+                    class="ma-n2"
+                  />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-text-field dense outlined label="Roll No." class="ma-n2" />
+                  <v-text-field
+                    v-model="newStudent.roll"
+                    :rules="[rules.required, rules.roll]"
+                    dense
+                    outlined
+                    label="Roll No."
+                    class="ma-n2"
+                  />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-text-field dense outlined label="Class" class="ma-n2" />
+                  <v-text-field
+                    v-model="newStudent.class"
+                    :rules="[rules.required, rules.class]"
+                    dense
+                    outlined
+                    label="Class"
+                    class="ma-n2"
+                  />
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-text-field dense outlined label="Section" class="ma-n2" />
+                  <v-text-field
+                    v-model="newStudent.section"
+                    :rules="[rules.required, rules.section]"
+                    dense
+                    outlined
+                    label="Section"
+                    class="ma-n2"
+                  />
                 </v-col>
               </v-row>
               <v-row justify="center">
                 <v-col cols="12" sm="8">
-                  <v-btn block> ADD STUDENT </v-btn>
+                  <v-btn block type="submit"> ADD STUDENT </v-btn>
                 </v-col>
               </v-row>
             </v-form>
@@ -45,13 +73,85 @@
             </v-list-item-content>
           </v-list-item>
         </v-list>
+
+        <v-snackbar
+          v-model="addedSuccessfuly"
+          top
+          transition="slide-y-transition"
+          timeout="1000"
+          color="green"
+        >
+          Success!
+        </v-snackbar>
+        <v-snackbar
+          v-model="recheckInput"
+          top
+          transition="slide-y-transition"
+          timeout="1000"
+          color="red"
+        >
+          Please recheck input.
+        </v-snackbar>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-export default {}
+/* eslint-disable no-console */
+export default {
+  data() {
+    return {
+      newStudent: {
+        name: '',
+        roll: '',
+        section: '',
+        class: '',
+      },
+      rules: {
+        required: (value) => !!value || 'Required',
+        name: (value) => {
+          if (value === undefined || value.length <= 2 || value.length > 30)
+            return 'Name should be between 3 and 30 characters'
+          else {
+            const pattern = /^[a-zA-Z]+[a-zA-Z\s-'.]*$/
+            return !!pattern.test(value) || 'Invalid characters in name'
+          }
+        },
+        roll: (value) => {
+          const pattern = /^[1-9][0-9]*$/
+          return (
+            !!pattern.test(value) ||
+            'Only numbers not starting with zero allowed'
+          )
+        },
+        class: (value) => {
+          const pattern = /^[1-9][0-2]*$/
+          if (!pattern.test(value)) return 'Class invalid'
+          else return true
+        },
+        section: (value) => {
+          const pattern = /^[A-Z]$/
+          if (!pattern.test(value))
+            return 'Only single uppercase letters please'
+          else if (value[0] < 'A' || value[0] > 'F') return 'Section invalid'
+          else return true
+        },
+      },
+      addedSuccessfuly: false,
+      recheckInput: false,
+    }
+  },
+  methods: {
+    addStudent() {
+      if (this.$refs.form.validate()) {
+        this.addedSuccessfuly = true
+      } else {
+        this.recheckInput = true
+      }
+    },
+  },
+}
 </script>
 
 <style></style>
